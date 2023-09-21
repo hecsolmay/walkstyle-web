8bc185699a66c551eac97fc2b54a3d8673cca14d
jeremdevzo11 <jeremimedina90@gmail.com>
2023-09-21 17:10:50 -0600

    change icon button

--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -4,7 +4,7 @@ type Direction = 'left' | 'right'
 
 interface IconButtonProps {
   direction?: Direction
-  children: React.ReactNode
+  children?: React.ReactNode
   text: string
   className?: string
   onclick?: () => void
@@ -14,16 +14,17 @@ export function IconButton ({
   text,
   onclick,
   direction = 'left',
+  className = '',
   children
 
 }: IconButtonProps) {
   return (
     <button
-      className='  mb-2 mr-2 inline-flex scale-90 cursor-pointer flex-row items-center justify-center gap-x-2 rounded-lg bg-red-500 px-5 py-2.5 text-center font-medium text-white transition-all duration-200 ease-in-out hover:opacity-100 hover:shadow-lg'
-      onClick={onclick}
-      style={{ width: '100%', height: '50%' }}>
-      {children}
+      className={`mb-2 mr-2 inline-flex h-12 w-full scale-90 cursor-pointer flex-row items-center justify-center gap-x-2 rounded-lg bg-red-500 px-5 py-2.5 text-center font-medium text-white transition-all duration-200 ease-in-out hover:opacity-100 hover:shadow-lg md:gap-3 ${className}`}
+      onClick={onclick} >
+      {direction === 'left' && children}
       {text}
+      {direction === 'right' && children}
     </button>
   )
 }
