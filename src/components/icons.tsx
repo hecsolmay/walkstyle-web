eae03e6759f1f9566c22d7ace582d7a7b3c56284
hecsolmay <hecsolmay@gmail.com>
2023-09-23 09:37:25 -0600

    use cn for default size in icons

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,37 +1,57 @@
+import { cn } from '@/utils/cn'
+
 interface IconProps {
   className?: string
 }
 
-export function SearchIcon ({ className = 'w-6 h-6' }: IconProps) {
+const defaultSize = 'w-6 h-6'
+
+export function SearchIcon ({ className = '' }: IconProps) {
   return (
-    <svg fill="none" className={className} stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" aria-hidden="true">
+    <svg fill="none" className={cn(defaultSize, className)} stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" aria-hidden="true">
       <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"></path>
     </svg>
   )
 }
 
-export function UserIcon ({ className = 'w-6 h-6' }: IconProps) {
+export function UserIcon ({ className = '' }: IconProps) {
   return (
-    <svg fill="none" className={className} stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" aria-hidden="true">
+    <svg fill="none" className={cn(defaultSize, className)} stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" aria-hidden="true">
       <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"></path>
     </svg>
   )
 }
 
-export function ShoppingCartIcon ({ className = 'w-6 h-6' }: IconProps) {
+export function ShoppingCartIcon ({ className = '' }: IconProps) {
   return (
-    <svg fill="none" className={className} stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" aria-hidden="true">
+    <svg fill="none" className={cn(defaultSize, className)} stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" aria-hidden="true">
       <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"></path>
     </svg>
   )
 }
 
-export function GoogleIcon ({ className = '<ww-5></ww-4> h-4' }: IconProps) {
+export function GoogleIcon ({ className = '' }: IconProps) {
   return (
-    <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 19"
-      className={className}>
+    <svg aria-hidden="true" fill="currentColor" viewBox="0 0 18 19"
+      className={cn(defaultSize, className)}>
       <path fillRule="evenodd" d="M8.842 18.083a8.8 8.8 0 0 1-8.65-8.948 8.841 8.841 0 0 1 8.8-8.652h.153a8.464 8.464 0 0 1 5.7 2.257l-2.193 2.038A5.27 5.27 0 0 0 9.09 3.4a5.882 5.882 0 0 0-.2 11.76h.124a5.091 5.091 0 0 0 5.248-4.057L14.3 11H9V8h8.34c.066.543.095 1.09.088 1.636-.086 5.053-3.463 8.449-8.4 8.449l-.186-.002Z" clipRule="evenodd"/>
     </svg>
 
+  )
+}
+
+export function HamburguerIcon ({ className = '' }: IconProps) {
+  return (
+    <svg fill="none" className={cn(defaultSize, className)} stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" aria-hidden="true">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"></path>
+    </svg>
+  )
+}
+
+export function XMarkIcon ({ className = '' }: IconProps) {
+  return (
+    <svg fill="none" className={cn(defaultSize, className)} stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" aria-hidden="true">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12"></path>
+    </svg>
   )
 }
