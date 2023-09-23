1b7e63ef5be03e57057291047f851464781299f8
jeremdevzo11 <jeremimedina90@gmail.com>
2023-09-23 00:07:07 -0600

    feact(creacion del footer)

--- src/app/(eccomerce)/layout.tsx
+++ src/app/(eccomerce)/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from '@/components/footer'
 import Navbar from '@/components/navbar'
 
 export default function EcommerceLayout ({
@@ -9,6 +10,7 @@ export default function EcommerceLayout ({
     <div className="flex min-h-screen flex-col">
       <Navbar />
       <main className='mx-6 my-7 flex-1 md:mx-8 lg:mx-12' >{children}</main>
+      <Footer/>
     </div>
   )
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+
+export default function Footer () {
+  return (
+    <footer className="flex h-44 flex-col  gap-5 bg-black p-8">
+      <div className="flex items-center gap-6">
+        <Image src={'/icon.webp'} width={55} height={55} alt='WalkStyle Logo' />
+        <h1 className="text-2xl font-bold uppercase text-white" >WALKSTYLE</h1>
+      </div>
+      <span className="border-t border-gray-400"></span>
+      <p className='text-slate-500'>© 2023 WalkStyle. All Rights Reserved.</p>
+    </footer>
+  )
+}
