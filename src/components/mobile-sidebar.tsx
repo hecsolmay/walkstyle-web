b8e9692696ea5fdb4a1263435b7b809b4c2adbe0
hecsolmay <hecsolmay@gmail.com>
2023-09-24 08:12:58 -0600

    add sidenavbar in mobile nav

--- src/components/mobile-sidebar.tsx
+++ src/components/mobile-sidebar.tsx
@@ -1,7 +1,9 @@
 'use client'
 
-import { HamburguerIcon } from '@/components/icons'
+import { HamburguerIcon, XMarkIcon } from '@/components/icons'
+import { links } from '@/contants/navlinks'
 import { cn } from '@/utils/cn'
+import Link from 'next/link'
 import { useState } from 'react'
 
 export function MobileSideBar () {
@@ -29,9 +31,19 @@ export function MobileSideBar () {
       </button>
 
       <div onClick={handleClick} className={cn('bg-black/50 absolute inset-0 z-10 h-screen w-screen ', !showSideBar && 'hidden')}>
-        <aside className='fixed left-0 top-0 z-20 h-screen w-[75vw] bg-white'>
-          <ul>
-            <li>Nav 1</li>
+        <aside className={cn('fixed left-0 top-0 z-20 h-screen w-[65vw]  rounded-tr-3xl bg-white transform transition-transform duration-500 ease-in-out ', showSideBar ? 'translate-x-0' : '-translate-x-full')}>
+          <div className='over flex h-20 items-center justify-between rounded-tr-3xl bg-teal-500 p-5 text-white'>
+            <h1 className='text-xl font-bold'>Walkstyle</h1>
+            <button onClick={handleCloseSideBar}>
+              <XMarkIcon className='h-7 w-7'/>
+            </button>
+          </div>
+          <ul className='flex h-[88%] flex-col gap-6 overflow-y-scroll px-5 py-6'>
+            {links.map(link => (
+              <li key={link.href} className={cn('text-2xl text-slate-400', link.divider !== undefined && 'border-b border-slate-400 pb-3')}>
+                <Link href={link.href} >{link.label}</Link>
+              </li>
+            ))}
           </ul>
         </aside>
       </div>
