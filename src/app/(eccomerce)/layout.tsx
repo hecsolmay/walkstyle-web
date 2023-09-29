33aaae1a88a31415941a685cbb30897b8dc3c435
hecsolmay <hecsolmay@gmail.com>
2023-09-28 19:19:01 -0600

    change margin in layout for eccomerce

--- src/app/(eccomerce)/layout.tsx
+++ src/app/(eccomerce)/layout.tsx
@@ -10,7 +10,8 @@ export default function EcommerceLayout ({
     <div className="flex h-screen min-h-screen w-screen flex-col">
       <Navbar />
       <div className='flex h-full w-full flex-col overflow-auto scroll-smooth scrollbar-thin scrollbar-track-transparent scrollbar-thumb-slate-400'>
-        <main className='mx-6 my-7 flex-1 md:mx-8 lg:mx-12' >
+        <main className='my-7 flex-1' >
+          {/* MARGIN TO ADD TO CONTAINER HEIGHT mx-6 md:mx-8 lg:mx-12 */}
           {children}
         </main>
         <Footer/>
