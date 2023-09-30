8d043f0e988708470dd90852ca90936c9f6af99d
hecsolmay <hecsolmay@gmail.com>
2023-09-30 09:53:03 -0600

    change eye icon in input

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,6 +1,8 @@
 'use client'
+
+import { EyeClosedIcon, EyeOpenIcon } from '@/components/icons'
+import { cn } from '@/utils/cn'
 import React, { useState } from 'react'
-import { EyeOpenIcon, EyeClosedIcon } from '@/components/icons'
 
 interface InputProps {
   label: string
@@ -39,7 +41,7 @@ export default function Input ({
         <input
           type={showPassword ? 'text' : type}
           name={name}
-          className={`block w-full rounded-lg border border-slate-300 bg-white p-2.5 text-sm text-gray-900 focus:outline focus:outline-blue-500 ${className}`}
+          className={cn('block w-full rounded-lg border border-slate-300 bg-white p-2.5 text-sm text-gray-900 focus:outline focus:outline-blue-500', className)}
           placeholder={placeholder}
           required={required}
           onChange={onChange}
@@ -48,9 +50,9 @@ export default function Input ({
           <button
             type='button'
             onClick={togglePasswordVisibility}
-            className='absolute inset-y-0 right-0 flex items-center px-3 focus:outline-none'
+            className='absolute inset-y-0 right-0 flex items-center px-3 text-slate-500 focus:outline-none'
           >
-            {showPassword ? <EyeOpenIcon /> : <EyeClosedIcon />}
+            {showPassword ? <EyeClosedIcon className='h-5 w-5'/> : <EyeOpenIcon className='h-5 w-5'/> }
           </button>
         )}
       </div>
