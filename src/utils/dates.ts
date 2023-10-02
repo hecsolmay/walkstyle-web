77aed98e9b75d64060b725e89a6ea328f1ce191d
hecsolmay <hecsolmay@gmail.com>
2023-10-02 09:11:14 -0600

    add utils date to get date in format day/month/year

---
+++ src/utils/dates.ts
@@ -0,0 +1,16 @@
+const Months = Object.freeze([
+  'enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio',
+  'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'
+]
+)
+export function getFormatedDate (date: string | Date) {
+  if (typeof date === 'string') {
+    date = new Date(Date.parse(date))
+  }
+
+  const day = date.getDate()
+  const month = Months[date.getMonth()]
+  const year = date.getFullYear()
+
+  return `${day}/${month}/${year}`
+}
