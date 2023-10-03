5fc1c164b150f7e4a4b6da9d86a9ee004102f821
hecsolmay <hecsolmay@gmail.com>
2023-10-03 00:34:03 -0600

    add props of products in total products component

--- src/app/(eccomerce)/cart/page.tsx
+++ src/app/(eccomerce)/cart/page.tsx
@@ -1,11 +1,43 @@
 import { TotalProducts } from '@/components/total-products'
 import { TotalToPay } from '@/components/total-to-pay'
+import { type ItemProduct } from '@/types/product'
+
+const product = {
+  name: 'Camisa de manga corta',
+  price: 10495,
+  gender: {
+    genderId: 'M',
+    name: 'Mujer'
+  },
+  brand: {
+    brandId: '1',
+    name: 'Nike'
+  },
+  imageUrl: 'https://tafmx.vtexassets.com/arquivos/ids/406372-192-auto',
+  description: 'Camisa de manga corta',
+  productId: '1'
+}
+
+const itemProduct: ItemProduct = {
+  product,
+  quantity: 1,
+  size: 32
+}
+
+const products = Array(15).fill(0).map(_ => ({
+  ...itemProduct,
+  product: {
+    ...product, productId: crypto.randomUUID()
+  }
+}))
 
 export default function CartPage () {
+  // AQUI VALIDAS SI EL LENGTH ES 0 Y MANDARIAS EL EMPTY STATE
+
   return (
     <div className="grid grid-cols-1 gap-6 md:grid-cols-3 ">
       <div className='col-span-2 mx-3 my-6 grid place-items-center'>
-        <TotalProducts/>
+        <TotalProducts products={products}/>
       </div>
       <div className='col-span-1 flex justify-center'>
         <TotalToPay/>
--- src/components/total-products.tsx
+++ src/components/total-products.tsx
@@ -2,36 +2,11 @@
 import ItemCardWithCounter from '@/components/item-cart-with-counter'
 import { type ItemProduct } from '@/types/product'
 
-const product = {
-  name: 'Camisa de manga corta',
-  price: 10495,
-  gender: {
-    genderId: 'M',
-    name: 'Mujer'
-  },
-  brand: {
-    brandId: '1',
-    name: 'Nike'
-  },
-  imageUrl: 'https://tafmx.vtexassets.com/arquivos/ids/406372-192-auto',
-  description: 'Camisa de manga corta',
-  productId: '1'
+interface TotalProductsProps {
+  products: ItemProduct[]
 }
 
-const itemProduct: ItemProduct = {
-  product,
-  quantity: 1,
-  size: 32
-}
-
-export function TotalProducts () {
-  const products = Array(15).fill(0).map(_ => ({
-    ...itemProduct,
-    product: {
-      ...product, productId: crypto.randomUUID()
-    }
-  }))
-
+export function TotalProducts ({ products }: TotalProductsProps) {
   return (
     <div className="flex h-[562px] w-full max-w-[827px] grow flex-col gap-4 overflow-y-scroll bg-slate-200 p-3 py-8 scrollbar-thin scrollbar-track-slate-300 scrollbar-thumb-slate-400 md:p-11">
       {products.map(product => (
