a1496a12a58b3d3438ce224e1e6a3cd93d04bbb0
hecsolmay <hecsolmay@gmail.com>
2023-10-03 17:51:29 -0600

    add product details page in e-commerce

--- src/app/(eccomerce)/product/[productId]/page.tsx
+++ src/app/(eccomerce)/product/[productId]/page.tsx
@@ -1,11 +1,13 @@
+import ProductImagesSlider from '@/components/product-images'
+import { SelectSizeWithCounter } from '@/components/select-size'
 import { type Product } from '@/types/product'
 
 const product: Product = {
   productId: '1',
-  name: 'Jordan',
+  name: 'Tenis Nike Revolution 6 Next Nature',
   brand: {
     brandId: '123',
-    name: 'Tenis Nike Revolution 6 Next Nature'
+    name: 'Jordan'
   },
   price: 999.00,
   imageUrl: 'https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/f8b6c5d4-f53d-4798-a833-7a7c8b063d66/calzado-air-max-intrlk-lite-Sp1WFC.png',
@@ -21,9 +23,37 @@ export default function ProductPage (
   { params }: { params: { productId: string } }
 ) {
   console.log({ params })
+  const exampleImages = [
+    product.imageUrl,
+    'https://e7.pngegg.com/pngimages/238/23/png-clipart-shoe-sneakers-nike-mercurial-vapor-tennis-women-shoes-orange-outdoor-shoe-thumbnail.png',
+    'https://e7.pngegg.com/pngimages/817/134/png-clipart-sports-shoes-nike-free-adidas-adizero-ubersonic-clay-damen-tennisschuh-tennis-court-60-feet-white-orange-thumbnail.png',
+    'https://e7.pngegg.com/pngimages/323/773/png-clipart-sneakers-basketball-shoe-sportswear-nike-shoe-outdoor-shoe-running-thumbnail.png'
+  ]
+
   return (
-    <div>
-      <h1>Product {product.productId}</h1>
+    <div className='mb-10 grid w-full grid-cols-1 p-4 md:h-[90vh] md:grid-cols-2 md:p-10'>
+      <div className='my-6 flex flex-col gap-6 md:hidden'>
+        <p className='text-xl uppercase'>{product.brand.name}</p>
+        <div className=' text-black'>
+          <h2 className='text-3xl font-bold'>{product.name}</h2>
+          <p className='mt-3 text-xl font-semibold'>{product.price.toLocaleString('es-MX', { style: 'currency', currency: 'MXN' })}</p>
+        </div>
+      </div>
+
+      <div className='grid place-content-center'>
+        <ProductImagesSlider images={exampleImages}/>
+      </div>
+      <div className='mt-5 flex flex-col justify-around gap-10 text-slate-600 md:max-w-[440px] md:gap-0 md:py-3'>
+        <div className='hidden flex-col gap-4 md:flex'>
+          <p className='text-xl uppercase'>{product.brand.name}</p>
+          <div className=' text-black'>
+            <h2 className='text-3xl font-bold'>{product.name}</h2>
+            <p className='mt-3 text-xl font-semibold'>{product.price.toLocaleString('es-MX', { style: 'currency', currency: 'MXN' })}</p>
+          </div>
+        </div>
+        <SelectSizeWithCounter sizes={product.sizes} />
+      </div>
     </div>
+
   )
 }
---
+++ src/components/product-images.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { cn } from '@/utils/cn'
+import { useState } from 'react'
+
+interface ProductImagesSliderProps {
+  images: string[]
+}
+
+export default function ProductImagesSlider ({ images }: ProductImagesSliderProps) {
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const currentImage = images[currentIndex]
+
+  const changeImage = (index: number) => {
+    if (index >= 0 && index < images.length) {
+      setCurrentIndex(index)
+    }
+  }
+
+  const hoverHandler = (index: number) => {
+    if (index >= 0 && index < images.length) {
+      setCurrentIndex(index)
+    }
+  }
+
+  return (
+    <div className='flex w-2/3 flex-col gap-4'>
+
+      <div className='grid h-96 w-80 place-content-center rounded-lg bg-slate-200 shadow-lg'>
+        <img className='h-64 w-64 object-contain mix-blend-darken' src={currentImage} />
+      </div>
+
+      <div className='grid w-80 grid-cols-4'>
+        {images.map((image, index) => (
+          <div key={image} onMouseOver={() => { hoverHandler(index) }} onClick={() => { changeImage(index) }} className={cn('h-16 w-16 cursor-pointer bg-slate-200', currentImage === image && 'border-2 border-amber-400')}>
+            <img className='h-16 w-16 object-contain mix-blend-darken' src={image} alt="" />
+          </div>
+
+        ))}
+
+      </div>
+    </div>
+  )
+}
