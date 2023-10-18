f702e3cd2c48e1dcc2ff834a0884e69c0b6f79b9
hecsolmay <hecsolmay@gmail.com>
2023-10-18 10:34:57 -0600

    fix build

--- src/app/admin/categories/page.tsx
+++ src/app/admin/categories/page.tsx
@@ -1,6 +1,7 @@
 import { AdminHeader } from '@/components/admin/admin-header'
 import { AdminTable } from '@/components/admin/admin-table'
 import CategoryRow from '@/components/admin/categories/category-row'
+import FormCreate from '@/components/admin/categories/form'
 import PaginationSection from '@/components/admin/pagination-section'
 import { getAdminCategories } from '@/services/categories'
 
@@ -15,6 +16,7 @@ export default async function CategoriesPage () {
   return (
     <div className='flex flex-col gap-8'>
       <AdminHeader modalClassName='grid place-content-center' buttonText='Add Category' searchPlaceholder='Search...' title='All Categories'>
+        <FormCreate />
       </AdminHeader>
       <AdminTable headers={['Logo', 'Name', 'Total Products', 'Status', 'Actions']}>
         {categories.map(category =>
