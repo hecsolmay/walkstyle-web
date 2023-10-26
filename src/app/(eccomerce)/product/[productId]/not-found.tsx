9ab31333b89fa2fa74b81b9eefee6eb59fc81cb1
heyitsGaspar <israel.chay.32@gmail.com>
2023-10-26 01:00:42 -0600

    empty states

--- src/app/(eccomerce)/product/[productId]/not-found.tsx
+++ src/app/(eccomerce)/product/[productId]/not-found.tsx
@@ -1,7 +1,24 @@
+import { LinkButton } from '@/components/link-button'
+import Image from 'next/image'
+
 export default function ProductNotFound () {
   return (
-    <div className="grid min-h-screen w-full place-items-center">
-      <h1>Product Not Found</h1>
+    <div className="grid h-screen place-content-center gap-8 bg-white px-4">
+      <div className="text-center">
+        <Image
+          src='/product-not-found.webp'
+          alt='product-not-found'
+          width={400}
+          height={400}
+        />
+
+        <h1 className="mt-6 text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl" >
+        Producto No Encontrado!!
+        </h1>
+
+        <p className="mt-4 text-gray-500">Intente otra busqueda diferente o similar.</p>
+      </div>
+      <LinkButton href='/' className='bg-teal-500 text-xl uppercase' text='Volver' />
     </div>
   )
 }
