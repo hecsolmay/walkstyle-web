3896736c809b11fe1bce54f7638b4ca2f163df7f
hecsolmay <hecsolmay@gmail.com>
2023-11-03 10:29:22 -0600

    add status loading in nav actions

--- src/components/nav-actions.tsx
+++ src/components/nav-actions.tsx
@@ -11,7 +11,7 @@ import Link from 'next/link'
 import { useEffect, useRef, useState } from 'react'
 
 export default function NavActions () {
-  const { data: session } = useSession()
+  const { data: session, status } = useSession()
 
   const userRef = useRef<HTMLButtonElement>(null)
   const menuRef = useRef<HTMLUListElement>(null)
@@ -40,51 +40,55 @@ export default function NavActions () {
     <div className='flex items-center gap-2 md:gap-5'>
 
       {/* USER PROFILE */}
+      {
+        status === 'loading'
+          ? <span style={{ height: '2rem', width: '2rem' }} className="loader"></span>
+          : <div className="relative flex items-center">
+            {
+              isLoggedIn
+                ? (
+                  <button ref={userRef} onClick={() => { setShowUser(!showUser) }} className='h-8 w-8 rounded-full text-center'>
+                    <img src={imageUrl} className="h-full w-full rounded-full object-contain" alt={session?.user?.name} />
+                  </button>)
+                : <button ref={userRef} className='cursor-pointer text-slate-600' onClick={() => { setShowUser(!showUser) }}>
+                  <UserIcon />
+                </button>
+            }
+            <div className={cn('absolute -left-14 top-9 w-[7.5rem] md:w-36 divide-y divide-gray-100 rounded-lg bg-slate-600 shadow ', showUser ? 'block' : 'hidden')}>
+              <ul ref={menuRef} className="py-2 text-sm text-gray-200" >
 
-      <div className="relative flex items-center">
-        {
-          isLoggedIn
-            ? (
-              <button ref={userRef} onClick={() => { setShowUser(!showUser) }} className='h-8 w-8 rounded-full text-center'>
-                <img src={imageUrl} className="h-full w-full rounded-full object-contain" alt={session?.user?.name} />
-              </button>)
-            : <button ref={userRef} className='cursor-pointer text-slate-600' onClick={() => { setShowUser(!showUser) }}>
-              <UserIcon />
-            </button>
-        }
-        <div className={cn('absolute -left-14 top-9 w-[7.5rem] md:w-36 divide-y divide-gray-100 rounded-lg bg-slate-600 shadow ', showUser ? 'block' : 'hidden')}>
-          <ul ref={menuRef} className="py-2 text-sm text-gray-200" >
+                {!isLoggedIn && (
+                  noSessionLinks.map((link) => (
+                    <li key={link.label}>
+                      <Link href={link.href} className={cn('block px-4 py-2 hover:bg-gray-600 hover:text-white', Boolean(link.divider) && 'border-b border-slate-400 pb-3')}>{link.label}</Link>
+                    </li>
+                  ))
+                )}
 
-            {!isLoggedIn && (
-              noSessionLinks.map((link) => (
-                <li key={link.label}>
-                  <Link href={link.href} className={cn('block px-4 py-2 hover:bg-gray-600 hover:text-white', Boolean(link.divider) && 'border-b border-slate-400 pb-3')}>{link.label}</Link>
-                </li>
-              ))
-            )}
+                {isLoggedIn && publicLinks.map((link) => (
+                  <li key={link.label}>
+                    <Link href={link.href} className={cn('block px-4 py-2 hover:bg-gray-600 hover:text-white', Boolean(link.divider) && 'border-b border-slate-400 pb-3')}>{link.label}</Link>
+                  </li>
+                ))}
 
-            {isLoggedIn && publicLinks.map((link) => (
-              <li key={link.label}>
-                <Link href={link.href} className={cn('block px-4 py-2 hover:bg-gray-600 hover:text-white', Boolean(link.divider) && 'border-b border-slate-400 pb-3')}>{link.label}</Link>
-              </li>
-            ))}
+                {isLoggedIn && isAdmin && protectedLinks.map((link) => (
+                  <li key={link.label}>
+                    <Link href={link.href} className={cn('block px-4 py-2 hover:bg-gray-600 hover:text-white', Boolean(link.divider) && 'border-b border-slate-400 pb-3')}>{link.label}</Link>
+                  </li>
+                ))}
 
-            {isLoggedIn && isAdmin && protectedLinks.map((link) => (
-              <li key={link.label}>
-                <Link href={link.href} className={cn('block px-4 py-2 hover:bg-gray-600 hover:text-white', Boolean(link.divider) && 'border-b border-slate-400 pb-3')}>{link.label}</Link>
-              </li>
-            ))}
+                {isLoggedIn && (
+                  <li className='mt-2 border-t border-slate-400'>
+                    <button onClick={async () => { await signOut({ callbackUrl: '/login' }) }} className='block px-4 py-2 hover:bg-gray-600 hover:text-white'>Cerrar Sesion</button>
+                  </li>
+                )}
 
-            {isLoggedIn && (
-              <li className='mt-2 border-t border-slate-400'>
-                <button onClick={async () => { await signOut({ callbackUrl: '/login' }) }} className='block px-4 py-2 hover:bg-gray-600 hover:text-white'>Cerrar Sesion</button>
-              </li>
-            )}
+              </ul>
+            </div>
 
-          </ul>
-        </div>
+          </div>
+      }
 
-      </div>
       {/* CART */}
 
       <ModalCartButton />
